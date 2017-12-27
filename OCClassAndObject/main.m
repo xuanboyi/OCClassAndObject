7cf8a3d336474add30e9b43f7a6be37a1870c864
玄博屹 <genhiroshi@xuanboyidemacbook-pro.local>
2017-12-27 19:44:46 +0800

    Initial Commit

---
+++ OCClassAndObject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCClassAndObject
+//
+//  Created by 玄博屹 on 2017/12/27.
+//  Copyright © 2017年 Xuan,boyi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
